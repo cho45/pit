6e7e0c0a453d635be224b4a90962d2e5ebadee7d
Keiji, Yoshimi <walf443@gmail.com>
2008-02-16 04:08:38 +0000

    lang/ruby/pit: change directory structure.
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/pit/trunk@6765 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/pit.rb
@@ -0,0 +1,97 @@
+
+require "yaml"
+require "pathname"
+require "tempfile"
+
+module Pit
+	VERSION   = "0.0.5"
+	Directory = Pathname.new("~/.pit").expand_path
+	@@config  = Directory + "pit.yaml"
+	@@profile = Directory + "default.yaml"
+
+	# Set _name_ setting to current profile.
+	# If not _opts_ specified, this opens $EDITOR with current profile setting.
+	# If `data` specified, this just sets it to current profile.
+	# If `config` specified, this opens $EDITOR with merged hash with specified hash and
+	# current profile.
+	def self.set(name, opts={})
+		profile = self.load
+		if opts.key?(:data)
+			result = opts[:data]
+		else
+			if ENV["EDITOR"].nil? || !$stdout.tty?
+				return {}
+			end
+			c = (opts[:config] || self.get(name)).to_yaml
+			t = Tempfile.new("pit")
+			t << c
+			t.close
+			system(ENV["EDITOR"], t.path)
+			t.open
+			result = t.read
+			if result == c
+				warn "No Changes"
+				return profile[name]
+			end
+			result = YAML.load(result)
+		end
+		profile[name] = result
+		@@profile.open("w") {|f| YAML.dump(profile, f) }
+		result
+	end
+
+	# Get _name_ setting from current profile.
+	# If not _opts_ specified, this just returns setting from current profile.
+	# If _require_ specified, check keys on setting and open $EDITOR.
+	def self.get(name, opts={})
+		ret = self.load[name] || {}
+		if opts[:require]
+			unless opts[:require].keys.all? {|k| ret[k] }
+				ret = opts[:require].update(ret)
+				ret = self.set(name, :config => ret)
+			end
+		end
+		ret || {"username"=>"", "password"=>""}
+	end
+
+	# Switch to current profile to _name_.
+	# Profile is set of settings. You can switch some settings using profile.
+	def self.switch(name, opts={})
+		@@profile = Directory + "#{name}.yaml"
+		config = self.config
+		ret = config["profile"]
+		config["profile"] = name
+		@@config.open("w") {|f| f << config.to_yaml }
+		ret
+	end
+
+	protected
+	def self.load
+		Directory.mkpath unless Directory.exist?
+		Directory.chmod 0700
+		unless @@config.exist?
+			@@config.open("w") {|f| f << {"profile"=>"default"}.to_yaml }
+			@@config.chmod(0600)
+		end
+		self.switch(self.config["profile"])
+		unless @@profile.exist?
+			@@profile.open("w") {|f| f << {}.to_yaml }
+			@@profile.chmod(0600)
+		end
+		YAML.load(@@profile.read) || {}
+	end
+
+	def self.config
+		YAML.load(@@config.read)
+	end
+end
+
+
+__END__
+p Pit.set("test")
+p Pit.get("test")
+
+config = Pit.get("twitter")
+p config["password"]
+p config["username"]
+
---
+++ test/pit_test.rb
@@ -0,0 +1,112 @@
+require "tmpdir"
+require "pathname"
+
+class Pathname
+	@@tempname_number = 0
+	def self.tempname(base=$0, dir=Dir.tmpdir)
+		@@tempname_number += 1
+		path = new(dir) + "#{File.basename(base)}.#{$$}.#{@@tempname_number}"
+		at_exit do
+			path.rmtree if path.exist?
+		end
+		path
+	end
+end
+
+dir  = Pathname.tempname
+dir.mkpath
+ENV["HOME"] = dir
+
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require "test/unit"
+class PitTest < Test::Unit::TestCase
+	def test_load
+		assert Pit
+	end
+
+	def test_set_get
+		Pit.set("test", :data => {"username"=>"foo","password"=>"bar"})
+		assert_equal "foo", Pit.get("test")["username"]
+		assert_equal "bar", Pit.get("test")["password"]
+
+		Pit.set("test2", :data => {"username"=>"foo2","password"=>"bar2"})
+		assert_equal "foo2", Pit.get("test2")["username"]
+		assert_equal "bar2", Pit.get("test2")["password"]
+
+		Pit.set("test", :data => {"username"=>"foo","password"=>"bar"})
+		assert_equal "foo", Pit.get("test")["username"]
+		assert_equal "bar", Pit.get("test")["password"]
+
+		assert_equal "foo", Pit.set("test", :data => {"username"=>"foo","password"=>"bar"})["username"]
+	end
+
+	def test_editor
+		ENV["EDITOR"] = nil
+		assert_nothing_raised("When editor is not set.") do
+			Pit.set("test")
+		end
+
+		tst = Pathname.tempname
+		exe = Pathname.tempname
+		exe.open("w") do |f|
+			f.puts <<-EOF.gsub(/^\t+/, "")
+				#!/usr/bin/env ruby
+
+				File.open(ENV["TEST_FILE"], "w") do |f|
+					f.puts ARGF.read
+				end
+			EOF
+		end
+		exe.chmod(0700)
+
+		ENV["TEST_FILE"] = tst.to_s
+		ENV["EDITOR"]    = exe.to_s
+		Pit.set("test")
+
+		assert_nothing_raised do
+			assert_equal({}, YAML.load_file(tst.to_s))
+		end
+
+		data = {
+			"foo" => "0101",
+			"bar" => "0202",
+		}
+
+		Pit.set("test", :data => data)
+		Pit.set("test")
+
+		assert_nothing_raised do
+			assert_equal(data, YAML.load_file(tst.to_s))
+		end
+
+		# clear
+		Pit.set("test", :data => {})
+		tst.open("w") {|f| }
+
+		Pit.get("test", :require => data)
+
+		assert_nothing_raised do
+			assert_equal(data, YAML.load_file(tst.to_s))
+		end
+	end
+
+	def test_switch
+		Pit.switch("default")
+		Pit.set("test", :data => {"username"=>"foo","password"=>"bar"})
+		assert_equal "foo", Pit.get("test")["username"]
+		assert_equal "bar", Pit.get("test")["password"]
+
+		r = Pit.switch("profile2")
+		assert_equal "default", r
+		assert_equal "profile2", Pit.config["profile"]
+		Pit.set("test", :data => {"username"=>"foo2","password"=>"bar2"})
+		assert_equal "foo2", Pit.get("test")["username"]
+		assert_equal "bar2", Pit.get("test")["password"]
+
+		Pit.switch("default")
+		Pit.set("test", :data => {"username"=>"foo","password"=>"bar"})
+		assert_equal "foo", Pit.get("test")["username"]
+		assert_equal "bar", Pit.get("test")["password"]
+	end
+end
